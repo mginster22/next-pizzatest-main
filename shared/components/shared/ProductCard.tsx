58b9d2023ea707d01a8f15142723450dfff45660
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-03 13:09:54 +0300

    sadsa

--- shared/components/shared/ProductCard.tsx
+++ shared/components/shared/ProductCard.tsx
@@ -40,7 +40,7 @@ export const ProductCard: React.FC<Props> = ({
         </p>
         <div className="flex justify-between items-center mt-4">
           <span className="text-[20px]">
-            от <b>{price} ₽</b>
+            от <b>{price} грн</b>
           </span>
 
           <Button variant="secondary">
