c30d56e3cda7443e0e4b97f7d7786641a937d8ae
Vlad Korochanskiy <mginster234942@gmail.com>
2025-05-03 14:23:47 +0300

    asdas

--- shared/services/instance.ts
+++ shared/services/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://next-pizzatest-main.vercel.app/api",
   withCredentials: true,
 });
